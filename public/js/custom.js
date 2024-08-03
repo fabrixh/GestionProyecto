2230a41daae0c515d6bd929c8958fa31c45eac69
David.-G93 <davidleogarcia993@gmail.com>
2024-08-02 22:21:13 -0300

    Corrigiendo estilos nav

--- public/js/custom.js
+++ public/js/custom.js
@@ -456,6 +456,25 @@ inputNumber($('.input-number'));
       }
       button.parent().parent().find('input').val(newVal);
   });
+
+  //javascript for navigation bar effect on scroll
+  window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle('down', window.scrollY > 0);
+
+
+  });
+
+  //javascript for responsive navigation sidebar menu
+  var menu = document.querySelector('.menu');
+  var menuBtn = document.querySelector('.menu-btn');
+  var closeBtn = document.querySelector('.close-btn');
+
+ 
+
+  closeBtn.addEventListener("click", () => {
+    menu.classList.remove('active');
+  });
   
 })(jQuery);
 
